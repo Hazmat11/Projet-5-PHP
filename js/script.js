1054c1d3d378c7cb4179661577fd8b4ef1b463b0
Hazmat <ebertrand@gaming.tech>
2021-11-24 17:37:37 +0100

    first

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var h = window.outerHeight;
+
+$(document).ready(function(){
+   $('.sidenav').sidenav();
+ });
+
+ $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+$(document).ready(function(){
+    $("#icon").click(function(){
+      $('body').addClass('animate');
+    });
+  });
+
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
