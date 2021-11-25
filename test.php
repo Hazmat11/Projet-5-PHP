7f389fbdc0348f5b77911d8b2201dfa06a4b78db
Hazmat <ebertrand@gaming.tech>
2021-11-25 11:15:47 +0100

    test

---
+++ test.php
@@ -0,0 +1,6 @@
+<form method="post" action="login.php">
+    <input type='email' name='email'>
+    <input type='password' name='password'>
+    <input type='text' name='first_name'>
+    <button type="submit" name="button"></button>
+</form>
