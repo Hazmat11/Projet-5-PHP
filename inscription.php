0034e78e5c4983d41d7bc907e773acf206c3bfc5
Hazmat <ebertrand@gaming.tech>
2021-11-25 12:14:12 +0100

    marche

--- inscription.php
+++ inscription.php
@@ -5,3 +5,10 @@
     <input type='text' name='login' />
     <input type='submit' value='Me connecter' />
 </form>
+
+<h1>Connexion</h1>
+<form method="post" action="login.php">
+<input type='email' name='email' />
+<input type='password' name='password' />
+<input type='submit' value='Me connecter' />
+</form>
