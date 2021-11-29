98be8729c571856945f7dc418821393b37b72bc9
Keziah <kbartolini@gaming.tech>
2021-11-29 12:59:19 +0100

    finition

--- envoi.php
+++ envoi.php
@@ -1,22 +1,22 @@
 <?php
 
-$msg = "Nom:\t$nom\n";
-$msg .= "E-Mail:\t$email\n";
-$msg .= "Message:\t$message\n\n";
+$msg = "Nom:\nom\n";
+$msg .= "E-Mail:\email\n";
+$msg .= "Message:\message\n\n";
 
 $recipient = "kbartolini@gaming.tech";
 $subject = "Formulaire";
 
 $mailheaders = "From: Mon test de formulaire<> \n";
-$mailheaders .= "Reply-To: $email\n\n";
+$mailheaders .= "Reply-To: email\n\n";
 
 mail($recipient, $subject, $msg, $mailheaders);
 
 echo "<HTML><HEAD>";
-echo "<TITLE>Formulaire envoyer!</TITLE></HEAD><BODY>";
-echo "<H1 align=center>Merci, $nom </H1>";
+echo "<TITLE>Email envoyé!</TITLE></HEAD><BODY>";
+echo "<H1 align=center>Merci </H1>";
 echo "<P align=center>";
-echo "Votre formulaire à bien été envoyé !</P>";
+echo "Votre email à bien été envoyé !</P>";
 echo "</BODY></HTML>";
 
 ?>
